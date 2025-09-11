987b9032fe68ea031239d7be765e8648f9f8e763
Vasyl <vasyl@example.com>
2025-09-11 20:46:51 +0300

    func 2

--- project/revision/main.py
+++ project/revision/main.py
@@ -1,15 +1,14 @@
-from revision.functions_utils import is_number_bigger_than_given
+from revision.functions_utils import is_number_bigger_than_given, add_salt_to_list
 
 
 def main():
-    result = is_number_bigger_than_given()
+    result = is_number_bigger_than_given(candidate_number=5)
     print(result)
-    result = is_number_bigger_than_given()
+
+    result = is_number_bigger_than_given(candidate_number=66, threshold=1)
     print(result)
-    result = is_number_bigger_than_given()
-    print(result)
-    result = is_number_bigger_than_given()
-    print(result)
+
+    add_salt_to_list([])
 
 
 if __name__ == "__main__":
