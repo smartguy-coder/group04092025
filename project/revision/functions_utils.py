5c59755cd4d72722fb5c05b1eda678b6f0357e21
Vasyl <vasyl@example.com>
2025-09-11 20:58:56 +0300

    func 3

--- project/revision/functions_utils.py
+++ project/revision/functions_utils.py
@@ -1,10 +1,13 @@
 from uuid import uuid4
 
+
 def is_number_bigger_than_given(candidate_number: float, threshold: float = 10) -> bool:
     """according to the task #6556565"""
     return candidate_number > threshold
 
 
 def add_salt_to_list(given_list: list) -> None:
-    print(uuid4())
-    # given_list.append()
+    """WARNING: list is being modified globally"""
+    identifier = uuid4().hex
+    print(identifier)
+    given_list.append(identifier)
--- project/revision/main.py
+++ project/revision/main.py
@@ -8,7 +8,13 @@ def main():
     result = is_number_bigger_than_given(candidate_number=66, threshold=1)
     print(result)
 
-    add_salt_to_list([])
+    given_list = []
+    add_salt_to_list(given_list)
+    add_salt_to_list(given_list)
+    add_salt_to_list(given_list)
+    add_salt_to_list(given_list)
+    add_salt_to_list(given_list)
+    print(given_list)
 
 
 if __name__ == "__main__":
