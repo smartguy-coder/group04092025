0a892d3fb09bfdb534ea08b6964d4591efc55c47
Vasyl <vasyl@example.com>
2025-09-11 20:19:10 +0300

    func 1

---
+++ project/revision/functions_utils.py
@@ -0,0 +1,2 @@
+def is_number_bigger_than_given() -> bool:
+    return True
---
+++ project/revision/main.py
@@ -0,0 +1,16 @@
+from revision.functions_utils import is_number_bigger_than_given
+
+
+def main():
+    result = is_number_bigger_than_given()
+    print(result)
+    result = is_number_bigger_than_given()
+    print(result)
+    result = is_number_bigger_than_given()
+    print(result)
+    result = is_number_bigger_than_given()
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
