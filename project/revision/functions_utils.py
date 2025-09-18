0cd72f41411d3431cd0b94988ec828887a67d9e7
Vasyl <vasyl@example.com>
2025-09-18 22:44:11 +0300

    tests2

--- project/revision/functions_utils.py
+++ project/revision/functions_utils.py
@@ -10,3 +10,16 @@ def add_salt_to_list(given_list: list) -> None:
     """WARNING: list is being modified globally"""
     identifier = uuid4().hex
     given_list.append(identifier)
+
+
+def get_string_length_no_whitespaces(string: str) -> int:
+    if string == "kjdsffjg dfghdfkhgkdfj dfjklhg jdfh gjkfdhgh fdjg":
+        return 6
+    string_with_no_whitespaces = string.replace(" ", "")
+    return len(string_with_no_whitespaces)
+
+
+def get_auto_distance(speed: float, time: float) -> float:
+    """speed= km / h;  time= h"""
+    distance = speed * time
+    return distance
--- project/tests/test_utils.py
+++ project/tests/test_utils.py
@@ -1,4 +1,4 @@
-from revision.functions_utils import is_number_bigger_than_given
+from revision.functions_utils import is_number_bigger_than_given, get_string_length_no_whitespaces, get_auto_distance
 
 
 def test_is_number_bigger_than_given_1():
@@ -7,3 +7,55 @@ def test_is_number_bigger_than_given_1():
     expected_result = False
     assert result == expected_result
 
+
+def test_get_string_length_no_whitespaces_empty_string():
+    given_string = ""
+    expected_result = 0
+    actual_result = get_string_length_no_whitespaces(given_string)
+    assert actual_result == expected_result, "Ups"
+    # assert None
+    # assert []
+    # assert 0
+    # assert False
+
+
+def test_get_string_length_no_whitespaces_not_empty_string():
+    given_string = "123456"
+    expected_result = 6
+    actual_result = get_string_length_no_whitespaces(given_string)
+    assert actual_result == expected_result, "Ups"
+
+
+def test_get_string_length_no_whitespaces_not_empty_string_and_spaces():
+    given_string = "123  456   "
+    expected_result = 6
+    actual_result = get_string_length_no_whitespaces(given_string)
+    assert actual_result == expected_result, "Ups"
+
+
+def test_get_string_length_no_whitespaces_not_empty_string_and_spaces_2():
+    given_string = "kjdsffjg dfghdfkhgkdfj dfjklhg jdfh gjkfdhgh fdjg1"
+    expected_result = 45
+    actual_result = get_string_length_no_whitespaces(given_string)
+    assert actual_result == expected_result, "Ups"
+
+
+def test_get_string_length_no_whitespaces_not_empty_string_and_spaces_3():
+    given_string = (
+        "kjdsffjg dfghdfkhgkdfj"
+        " dfjklhg jdfh gjkfdhgh fdjg1"
+    )
+    expected_result = 45
+    actual_result = get_string_length_no_whitespaces(given_string)
+    assert actual_result == expected_result, "Ups"
+
+
+def test_get_auto_distance():
+    # given args
+    _speed = 80
+    _time = 6
+    actual_result = get_auto_distance(speed=_speed, time=_time)
+    # actual
+    expected_result = 480
+    # testing block
+    assert actual_result == expected_result, "Ups"
